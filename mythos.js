d2f5db7049e8647b81a2263c1a05863fbf1bff2b
Max <silverhammermba@gmail.com>
2017-11-04 21:39:33 -0400

    add cards, fix id mixup

--- mythos.js
+++ mythos.js
@@ -536,7 +536,6 @@ function draw(autodiscard, dosave) {
 		card.innerHTML = '<button type="button" class="action" onclick="storm()" id="storm">Draw a <span class="rumor">Rumor</span> Mythos card.</button>';
 	}
 
-
 	// add the card
 	div.insertBefore(card, div.firstChild);
 
@@ -653,6 +652,17 @@ function newgame() {
 	document.getElementById('draw').disabled = "";
 }
 
+/* card format is
+ * COLOR-ID-DIFFICULTY EXPANSION ELDRITCH CLUES
+ * with spaces removed
+ *
+ * COLOR:      blue/yelw/gren
+ * ID:         unique ID to avoid name conflicts (not used internally)
+ * DIFFICULTY: E/N/H for easy/normal/hard
+ * EXPANSION:  single letter code indicating expansion (see index.html)
+ * ELDRITCH:   (optional) number of starting eldritch tokens, or - for ongoing but no tokens
+ * CLUES:      (optional) a lowercase c if clues can be placed on the card
+ */
 var cards = ['blue-00-HR4c', 'blue-01-HR2', 'blue-02-NR-', 'blue-03-NR4',
 'blue-04-ER3', 'blue-05-NM3', 'blue-06-EM5', 'blue-07-HM-', 'blue-08-HM3',
 'blue-09-NM-', 'blue-10-NM4', 'blue-11-HL3', 'blue-12-NL-', 'blue-13-EB3',
@@ -661,33 +671,38 @@ var cards = ['blue-00-HR4c', 'blue-01-HR2', 'blue-02-NR-', 'blue-03-NR4',
 'blue-24-EB4', 'blue-25-ED-', 'blue-26-ND3', 'blue-27-ND3', 'blue-28-ND3c',
 'blue-29-HD-', 'blue-30-HD3', 'blue-31-NC-c', 'blue-32-HC2', 'blue-33-HC5',
 'blue-34-NC3', 'blue-35-NC1', 'blue-36-HP-', 'blue-37-EP-', 'blue-38-NP4',
-'blue-39-EP3', 'blue-40-HP0', 'blue-41-NP4', 'blue-42-EC4', 'gren-00-ER',
-'gren-01-ER', 'gren-02-NR-', 'gren-03-NR-', 'gren-04-NR', 'gren-05-HR-',
-'gren-06-HR', 'gren-07-EP-', 'gren-08-EP', 'gren-09-NP', 'gren-10-NP',
-'gren-11-HM', 'gren-12-NM', 'gren-13-NL', 'gren-14-HM', 'gren-15-HL',
-'gren-16-NB', 'gren-17-EB', 'gren-18-EB', 'gren-19-HB-', 'gren-20-NB',
-'gren-21-NB-', 'gren-22-EB', 'gren-23-HM', 'gren-24-EB', 'gren-25-HB',
-'gren-26-EB', 'gren-27-HB', 'gren-28-NB', 'gren-29-HB', 'gren-30-NB',
-'gren-31-HB', 'gren-32-NB', 'gren-33-NB-', 'gren-34-EM', 'gren-35-NB',
-'gren-36-EM', 'gren-37-NM', 'gren-38-NM-', 'gren-39-NM-', 'gren-40-ED',
-'gren-41-ED', 'gren-42-ND', 'gren-43-ND-', 'gren-44-ND-', 'gren-45-HD',
-'gren-46-HD', 'gren-47-HD', 'gren-48-NC-', 'gren-49-NC', 'gren-50-NC',
-'gren-51-EC', 'gren-52-EC', 'gren-54-HC-', 'gren-55-HC', 'gren-56-NC-',
-'gren-59-HP', 'gren-60-HP', 'gren-61-HP-', 'gren-62-NP', 'yelw-00-NR',
-'yelw-01-ER', 'yelw-02-ER', 'yelw-03-HR', 'yelw-04-NR', 'yelw-05-NR',
-'yelw-06-HR', 'yelw-07-HR', 'yelw-08-HP', 'yelw-09-NP', 'yelw-10-NP',
-'yelw-11-NP', 'yelw-12-HP', 'yelw-13-HP', 'yelw-14-NM', 'yelw-15-HM',
-'yelw-16-HM', 'yelw-17-HM', 'yelw-18-HM', 'yelw-19-EM', 'yelw-20-NM',
-'yelw-21-EM', 'yelw-22-EM', 'yelw-23-NM', 'yelw-24-NM', 'yelw-25-NL',
-'yelw-26-HL', 'yelw-27-NB', 'yelw-28-HB', 'yelw-29-HB', 'yelw-30-NB',
-'yelw-31-NB', 'yelw-32-NB', 'yelw-33-NB', 'yelw-34-EB', 'yelw-35-HB',
-'yelw-36-EB', 'yelw-37-EB', 'yelw-38-HB', 'yelw-39-NB', 'yelw-40-NB',
-'yelw-41-NB', 'yelw-42-EB', 'yelw-43-NB', 'yelw-44-HB', 'yelw-45-EB',
-'yelw-46-NB', 'yelw-47-NB', 'yelw-48-ED', 'yelw-49-ED', 'yelw-50-ND',
-'yelw-51-ND', 'yelw-52-ND', 'yelw-53-ND', 'yelw-54-ND', 'yelw-55-HD',
-'yelw-56-HD', 'yelw-57-HD', 'yelw-58-NC', 'yelw-59-NC', 'yelw-60-NC',
-'yelw-61-EC', 'yelw-62-EC', 'yelw-63-HC', 'yelw-64-HC', 'yelw-65-HC',
-'yelw-66-NC', 'yelw-67-NC', 'yelw-68-EP', 'yelw-69-EP'];
+'blue-39-EP3', 'blue-40-HP0', 'blue-41-NP4', 'blue-42-EC4', 'blue-43-HS-',
+'blue-44-NS3', 'blue-45-ES3', 'gren-00-ER', 'gren-01-ER', 'gren-02-NR-',
+'gren-03-NR-', 'gren-04-NR', 'gren-05-HR-', 'gren-06-HR', 'gren-07-EP-',
+'gren-08-EP', 'gren-09-NP', 'gren-10-NP', 'gren-11-HM', 'gren-12-NM',
+'gren-13-NL', 'gren-14-HM', 'gren-15-HL', 'gren-16-NB', 'gren-17-EB',
+'gren-18-EB', 'gren-19-HB-', 'gren-20-NB', 'gren-21-NB-', 'gren-22-EB',
+'gren-23-HM', 'gren-24-EB', 'gren-25-HB', 'gren-26-EB', 'gren-27-HB',
+'gren-28-NB', 'gren-29-HB', 'gren-30-NB', 'gren-31-HB', 'gren-32-NB',
+'gren-33-NB-', 'gren-34-EM', 'gren-35-NB', 'gren-36-EM', 'gren-37-NM',
+'gren-38-NM-', 'gren-39-NM-', 'gren-40-ED', 'gren-41-ED', 'gren-42-ND',
+'gren-43-ND-', 'gren-44-ND-', 'gren-45-HD', 'gren-46-HD', 'gren-47-HD',
+'gren-48-NC-', 'gren-49-NC', 'gren-50-NC', 'gren-51-EC', 'gren-52-EC',
+'gren-54-HC-', 'gren-55-HC', 'gren-56-NC-', 'gren-59-HP', 'gren-60-HP',
+'gren-61-HP-', 'gren-62-NP', 'gren-63-HS', 'gren-64-HS-', 'gren-65-NS',
+'gren-66-NS', 'gren-67-NS-', 'gren-68-NS', 'gren-69-HS-', 'gren-70-ES',
+'gren-71-ES-', 'yelw-00-NR', 'yelw-01-ER', 'yelw-02-ER', 'yelw-03-HR',
+'yelw-04-NR', 'yelw-05-NR', 'yelw-06-HR', 'yelw-07-HR', 'yelw-08-HP',
+'yelw-09-NP', 'yelw-10-NP', 'yelw-11-NP', 'yelw-12-HP', 'yelw-13-HP',
+'yelw-14-NM', 'yelw-15-HM', 'yelw-16-HM', 'yelw-17-HM', 'yelw-18-HM',
+'yelw-19-EM', 'yelw-20-NM', 'yelw-21-EM', 'yelw-22-EM', 'yelw-23-NM',
+'yelw-24-NM', 'yelw-25-NL', 'yelw-26-HL', 'yelw-27-NB', 'yelw-28-HB',
+'yelw-29-HB', 'yelw-30-NB', 'yelw-31-NB', 'yelw-32-NB', 'yelw-33-NB',
+'yelw-34-EB', 'yelw-35-HB', 'yelw-36-EB', 'yelw-37-EB', 'yelw-38-HB',
+'yelw-39-NB', 'yelw-40-NB', 'yelw-41-NB', 'yelw-42-EB', 'yelw-43-NB',
+'yelw-44-HB', 'yelw-45-EB', 'yelw-46-NB', 'yelw-47-NB', 'yelw-48-ED',
+'yelw-49-ED', 'yelw-50-ND', 'yelw-51-ND', 'yelw-52-ND', 'yelw-53-ND',
+'yelw-54-ND', 'yelw-55-HD', 'yelw-56-HD', 'yelw-57-HD', 'yelw-58-NC',
+'yelw-59-NC', 'yelw-60-NC', 'yelw-61-EC', 'yelw-62-EC', 'yelw-63-HC',
+'yelw-64-HC', 'yelw-65-HC', 'yelw-66-NC', 'yelw-67-NC', 'yelw-68-EP',
+'yelw-69-EP', 'yelw-70-HS', 'yelw-71-HS', 'yelw-72-HS', 'yelw-73-NS',
+'yelw-74-NS', 'yelw-75-NS', 'yelw-76-NS', 'yelw-77-HS', 'yelw-78-NS',
+'yelw-79-ES', 'yelw-80-ES', 'yelw-81-ES'];
 
 // most you need is 10 green, 11 yellow, 3 blue (for starting rumor)
 var ancient_ones = {
