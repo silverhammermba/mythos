5b242a8c18cd2acf24f42f57294b6140e04d70e9
Maxwell Anselm <manselm@lutron.com>
2023-04-15 13:45:11 -0400

    fill out expansions

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -5,6 +5,12 @@ function Setup() {
   const expansions = [
     { name: 'Forsaken Lore', abbr: 'L', state: useState(false) },
     { name: 'Mountains of Madness', abbr: 'M', state: useState(false) },
+    { name: 'Strange Remnants', abbr: 'R', state: useState(false) },
+    { name: 'Under the Pyramids', abbr: 'P', state: useState(false) },
+    { name: 'Signs of Carcosa', abbr: 'C', state: useState(false) },
+    { name: 'The Dreamlands', abbr: 'D', state: useState(false) },
+    { name: 'Cities in Ruin', abbr: 'S', state: useState(false) },
+    { name: 'Masks of Nyarlathotep', abbr: 'N', state: useState(false) },
   ];
 
   return (
