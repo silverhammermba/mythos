87fa5c150e98c75cd0eb8d7c7a3682c5ef0e0177
Maxwell Anselm <manselm@lutron.com>
2023-04-15 15:42:24 -0400

    add ancient ones

---
+++ src/components/AncientOneSelect.tsx
@@ -0,0 +1,27 @@
+interface AncientOneSelectProps {
+  ancientOnes: string[],
+  selected: string | undefined,
+  onChange: (selection: string) => void
+}
+
+function AncientOneSelect({ ancientOnes, selected, onChange }: AncientOneSelectProps) {
+  return (
+    <div className="ancient-one-select-component">
+      <label htmlFor="ao">
+        Ancient One
+        <select id="ao" value={selected} onChange={(event) => onChange(event.currentTarget.value)}>
+          {ancientOnes.map((ancientOne) => (
+            <option
+              key={ancientOne}
+              value={ancientOne}
+            >
+              {ancientOne}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default AncientOneSelect;
--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -1,31 +1,52 @@
 import { useState } from 'react';
+import AncientOneSelect from './AncientOneSelect';
 import ExpansionSelect from './ExpansionSelect';
 import content from '../content';
 
 function Setup() {
   const numExpansions = content.length;
   const initialExpansionState = Array(numExpansions).fill(false);
+  // base set is selected by default
+  initialExpansionState[0] = true;
 
-  const [expansionState, setExpansionState] = useState(initialExpansionState);
+  const [expansions, setExpansions] = useState<boolean[]>(initialExpansionState);
+
+  const [ancientOne, setAncientOne] = useState<string | undefined>(undefined);
+
+  const ancientOnes = content
+    .filter((pack, index) => expansions[index])
+    .flatMap((pack) => pack.ancientOnes)
+    .sort();
 
   return (
     <div className="setup-component">
       <form>
         <fieldset id="expansions">
           <legend>Expansions</legend>
-          {content.map((expansion, index) => (
-            <ExpansionSelect
-              key={expansion.name}
-              name={expansion.name}
-              selected={expansionState[index]}
-              onChange={(selected) => setExpansionState((prev) => {
-                const newSelection = [...prev];
-                newSelection[index] = selected;
-                return newSelection;
-              })}
-            />
-          ))}
+          {content.map((expansion, index) => {
+            if (index === 0) return null;
+            return (
+              <ExpansionSelect
+                key={expansion.name}
+                name={expansion.name}
+                selected={expansions[index]}
+                onChange={(selected: boolean) => {
+                  setExpansions((prev) => {
+                    const newSelection = [...prev];
+                    newSelection[index] = selected;
+                    newSelection[0] = true; // never deselect base set
+                    return newSelection;
+                  });
+                }}
+              />
+            );
+          })}
         </fieldset>
+        <AncientOneSelect
+          ancientOnes={ancientOnes}
+          selected={ancientOne}
+          onChange={setAncientOne}
+        />
       </form>
     </div>
   );
