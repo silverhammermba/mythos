ce6f678895207b33bb6fb34655e0b274685bd97c
Maxwell Anselm <manselm@lutron.com>
2023-04-23 22:57:35 -0400

    maybe finish normal deck build function

--- src/components/reducers/deck.ts
+++ src/components/reducers/deck.ts
@@ -1,4 +1,4 @@
-enum CardType {
+enum CardColor {
   Green,
   Yellow,
   Blue,
@@ -12,7 +12,7 @@ enum CardDifficulty {
 
 interface Card {
   id: string,
-  color: CardType,
+  color: CardColor,
   difficulty: CardDifficulty,
   eldritch: number | undefined,
   clues: number | undefined,
@@ -22,9 +22,7 @@ interface Card {
 interface Deck {
   discard: Card[],
   active: Card[],
-  stage1: Card[],
-  stage2: Card[],
-  stage3: Card[],
+  stages: Card[][],
   box: Card[],
 }
 
@@ -34,6 +32,7 @@ enum DeckActionType {
   Draw,
 }
 
+// shuffle array in place
 function shuffle<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -41,6 +40,29 @@ function shuffle<T>(array: T[]): T[] {
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
+}
+
+// remove first count elements satisfying predicate from array in-place and return them
+function remove<T>(
+  array: T[],
+  count: number,
+  predicate: (value: T, index: number | undefined, array: T[] | undefined) => Boolean,
+): T[] {
+  const indices: number[] = [];
+  const removed: T[] = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (predicate(array[i], i, array)) {
+      indices.push(i);
+      removed.push(array[i]);
+    }
+  }
+
+  for (let i = indices.length - 1; i >= 0; i -= 1) {
+    array.splice(i, 1);
+  }
+
+  return removed;
 }
 
 export type DeckAction =
@@ -53,18 +75,54 @@ export const deckReducer = (state: Deck, action: DeckAction): Deck => {
     case DeckActionType.SetBox:
       return { ...state, box: action.cards };
     case DeckActionType.Build: {
-      const box = [...state.box];
-      const green = shuffle(state.box.filter((card) => card.color === CardType.Green));
-      const yellow = shuffle(state.box.filter((card) => card.color === CardType.Yellow));
-      const blue = shuffle(state.box.filter((card) => card.color === CardType.Blue));
+      // shuffle the cards so that they're already random when building stages
+      const box = shuffle([...state.box]);
 
-      const stage1: Card[] = [];
-      stage1.push(green.pop());
-      const stage2: Card[] = [];
-      const stage3: Card[] = [];
-      action.counts[0]
+      // ensure that active cards are not in the box (never should have duplicates)
+      action.active.forEach((activeCard) => {
+        remove(box, 1, (card: Card) => card.id === activeCard.id);
+      });
 
-      return state;
+      const numStages = 3;
+      const colors = [CardColor.Green, CardColor.Yellow, CardColor.Blue];
+
+      const stages: Card[][] = [];
+      const missing: { type: string, count: number }[] = [];
+
+      if (action.counts.length !== numStages * colors.length) {
+        console.error(`wrong number of ancient one deck stage counts: ${action.counts.length}`);
+      }
+
+      for (let s = 0; s < action.counts.length; s += colors.length) {
+        const stage: Card[] = [];
+
+        for (let i = 0; i < colors.length && s + i < action.counts.length; i += 1) {
+          const color = colors[i];
+          const criteria = CardColor[color];
+          const count = action.counts[s + i];
+
+          const drawn = remove(box, count, (card: Card) => card.color === color);
+          const numberMissing = count - drawn.length;
+
+          if (numberMissing) {
+            missing.push({
+              type: criteria,
+              count: numberMissing,
+            });
+          } else {
+            stage.push(...drawn);
+          }
+        }
+
+        stages.push(shuffle(stage));
+      }
+
+      return {
+        discard: [],
+        active: action.active,
+        stages,
+        box,
+      };
     }
     case DeckActionType.Draw:
       return state;
