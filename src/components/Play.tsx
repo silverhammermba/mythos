469a398e2706450a7636ff981c692a115e0c33ef
Maxwell Anselm <manselm@lutron.com>
2023-09-02 15:17:38 -0400

    forgot to add new component

---
+++ src/components/Play.tsx
@@ -0,0 +1,24 @@
+import { Dispatch } from 'react';
+import { DeckAction } from './reducers/deck';
+import { Deck } from '../types/deck';
+
+interface PlayProp {
+  deck: Deck,
+  dispatch: Dispatch<DeckAction>,
+}
+
+function Play({
+  deck,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  dispatch,
+}: PlayProp) {
+  return (
+    <div className="play-component">
+      <p>
+        {`Difficulty ${deck.difficulty}`}
+      </p>
+    </div>
+  );
+}
+
+export default Play;
